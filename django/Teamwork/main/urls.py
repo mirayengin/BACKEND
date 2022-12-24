b18eef80524e6a77f0492dd960a5371d5d22f3df
erhan <erhanserkansabriye@gmail.com>
2022-12-24 17:42:04 +0300

    feat: url leler ayarlandı

--- django/Teamwork/main/urls.py
+++ django/Teamwork/main/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import home
 from django.conf import settings
 from django.conf.urls.static import static
 # from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('task/', include("task.urls")),
 ]
