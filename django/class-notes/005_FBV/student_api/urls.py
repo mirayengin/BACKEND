a6084db692f4b937f90deddf1526b330066eedda
erhan <erhanserkansabriye@gmail.com>
2022-12-24 14:13:32 +0300

    feat:crud işlemleri yapıldı

--- django/class-notes/005_FBV/student_api/urls.py
+++ django/class-notes/005_FBV/student_api/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path
-from .views import home , student_api, student_api_get_update_delete, path_api
+from .views import(
+ home,
+# students_list,
+# student_create,
+# student_detail,
+# student_update,
+# student_delete,
+student_api,
+student_api_get_update_delete,
+
+)
 
 urlpatterns = [
     path("", home),
-    path("student/", student_api),
-    path("student/<int:pk>", student_api_get_update_delete, name="detail"),
-    path("path/", path_api)
+    # path("students_list/", students_list, name='list'),
+    # path("students_create/", student_create, name='create'),
+    # path("students_detail/<int:pk>/", student_detail, name='detail'),
+    # path("students_update/<int:pk>/", student_update, name='update'),
+    # path("students_delete/<int:pk>/", student_delete, name='delete')
+
 ]
--- django/class-notes/005_FBV/student_api/views.py
+++ django/class-notes/005_FBV/student_api/views.py
@@ -7,13 +7,77 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-# Create your views here.
 
-
-
-
+#! Bununla json formata çeviriyoruz ve HttpResponse kullanmıyoruz. Hangi meodu kullanaçağımızı belirtiyoruz. defaılt  get tir
+@api_view()
 def home(request):
-    return HttpResponse('<h1>API Page</h1>')
+  return Response({'home':'This is home page...'})
+
+  #? http methods ------------> get, post, delete, put, patch
+ #! GET: Sunucudan bir belge veya veri almak için kullanılır. GET isteği yalnızca veriyi okumak için kullanılmalıdır, çünkü GET isteği tarayıcı ve proxy sunucuları tarafından önbelleğe alınabilir ve bu nedenle birkaç kez çalıştırılabilir.
+#?HEAD: Sunucudan bir belge veya verinin üstbilgilerini almak için kullanılır. HEAD isteği, GET isteğine benzerdir, ancak sunucudan geri dönen veri yoktur. Bu, sunucudaki bir belgenin varlığını veya önbelleklenme durumunu kontrol etmek için kullanılabilir.
+#!POST: Sunucuya bir belge veya veri göndermek için kullanılır. POST isteği, veritabanı güncelleştirmesi gibi veri oluşturan ve değiştiren işlemler için kullanılır.
+#?PUT: Sunucuya bir belge veya veri yüklemek için kullanılır. PUT isteği, bir belge veya verinin tamamının yerine koyulması için kullanılır.
+#!DELETE: Sunucudaki bir belge veya veriyi silmek için kullanılır.
+#?CONNECT: Sunucuyla bir iletişim kanalı oluşturmak için kullanılır.
+#!OPTIONS: Sunucunun desteklediği HTTP metotlarının bir listesini almak için kullanılır.
+#?TRACE: Sunucudan gönderilen bir isteğin yolunu takip etmek için kullanılır.
+#!PATCH: Sunucudaki bir belge veya verinin bir parçasını değiştirmek için kullanılır.
+
+@api_view(['GET'])
+def students_list(request):
+  students = Student.objects.all()
+  print(students)
+  #! Birden fazla data döneçeği için many=True diyoruz
+  serializer = StudentSerializer(students, many=True)
+  print(serializer)
+  print(serializer.data)
+  return Response(serializer.data)
+
+
+
+@api_view(['POST'])
+def student_create(request):
+  serializer = StudentSerializer(data=request.data)
+  if serializer.is_valid():
+    serializer.save()
+    message = {"message":f"Student updated successfull..."}
+    return Response(message,status.HTTP_201_CREATED)
+  return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  
+
+@api_view(['GET'])
+def student_detail(request,pk):
+  # student = Student.objects.get(id=pk)
+  student = get_object_or_404(Student, id=pk)
+  serializer = StudentSerializer(student)
+  return Response(serializer.data)
+
+
+@api_view(['PUT'])
+def student_update(request,pk):
+  student = get_object_or_404(Student, id=pk)
+  serializer = StudentSerializer(instance=student, data=request.data, )
+  if serializer.is_valid():
+    serializer.save()
+    message = {"message":f"Student updated successfull..."}
+    return Response(message)
+  return Response(serializer.data)
+
+
+
+@api_view(['DELETE'])
+def student_delete(request,pk):
+  student = get_object_or_404(Student, id=pk)
+  student.delete()
+  message = {
+    'message': 'Student deleted succesfull...'
+  }
+  return Response(message)
+
+
+
+  #!Bir yerde toplanmış hali
 
 @api_view(['GET', 'POST'])
 def student_api(request):
@@ -47,6 +111,7 @@ def student_api_get_update_delete(request, pk):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'PATCH':
         serializer = StudentSerializer(student, data=request.data,partial=True)
+        #!parça parça güncelleme işlemleri için kullanılan method. PATCH için.
         if serializer.is_valid():
             serializer.save()
             data = {
@@ -60,25 +125,3 @@ def student_api_get_update_delete(request, pk):
             "message": f"Student {student.last_name} deleted successfully"
         }
         return Response(data)
-    
-
-@api_view(['GET', 'POST'])
-def path_api(request):
-    # from rest_framework.decorators import api_view
-    # from rest_framework.response import Response
-    # from rest_framework import status
-
-    if request.method == 'GET':
-        paths = Path.objects.all()
-        serializer = PathSerializer(paths, many=True, context={'request': request})
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        # from pprint import pprint
-        # pprint(request)
-        serializer = PathSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            data = {
-                "message": f"Path saved successfully!"}
-            return Response(data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
