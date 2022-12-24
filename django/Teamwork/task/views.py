493e49ebd22d1d12cb6703ca6f657134cb8a4953
erhan <erhanserkansabriye@gmail.com>
2022-12-24 16:59:09 +0300

    feat: image ler için media url eklendi

--- django/Teamwork/task/views.py
+++ django/Teamwork/task/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+#!SOAP (Simple Object Access Protocol), yani Basit Nesne Erişim Protokolü daha sıkı bir güvenlik yapısıyla veri transferini sağlayan API mimarisidir. Bu mimaride veri akışı XML formatı kullanılarak sağlanır. Yapılandırması Rest API’ye göre daha zor olsa da bu mimari daha güvenli bir bağlantı sağladığı söylenir.
+
