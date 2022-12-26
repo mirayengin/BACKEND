9900c57b050c5ee3709ca6c2e790e9f043bf7525
erhan <erhanserkansabriye@gmail.com>
2022-12-26 20:15:20 +0300

    feat: proje ayağa kaldırıldı

--- django/class-notes/006_CBV/main/views.py
+++ django/class-notes/006_CBV/main/views.py
@@ -2,3 +2,28 @@ from django.http import HttpResponse
 
 def real_home(request):
     return HttpResponse('This main home page......')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+#?kurmak için sırasıyla;
+#?python -m venv env
+#!source env/Scripts/Activate (windows-bash için)
+#?pip install -r requirements.txt
+#?python manage.py createsuperuser
+#!python manage.py runserver
