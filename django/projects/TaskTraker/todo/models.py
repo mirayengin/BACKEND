3f9779e0049d62d279346570e279612a5706459d
erhan <erhanserkansabriye@gmail.com>
2022-12-27 20:59:18 +0300

    feat:proje yüklendi ve model oluş. ve veri girildi

---
+++ django/projects/TaskTraker/todo/models.py
@@ -0,0 +1,105 @@
+from django.db import models
+
+# Create your models here.
+
+class Todo(models.Model):
+  PRIORITY = {
+    (1,'High'),
+    (2,'Medium'),
+    (3,'Low')
+  }
+  task = models.CharField(max_length=50)
+  description = models.TextField(blank=True)
+  priority = models.SmallIntegerField(choices=PRIORITY, default=3)
+  is_done = models.BooleanField(default=False)
+  update_date = models.DateTimeField(auto_now=True)
+  create_date = models.DateTimeField(auto_now_add=True) #birkez eklenecek
+
+
+  def __str__(self):
+    return self.task
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
