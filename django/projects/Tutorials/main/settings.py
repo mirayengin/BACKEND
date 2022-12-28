c36a30acb4308dfdd35dd5eee4f0fb7a5ae344bd
erhan <erhanserkansabriye@gmail.com>
2022-12-28 12:44:27 +0300

    feat: view ve urls ler oluşturuldu

--- django/projects/Tutorials/main/settings.py
+++ django/projects/Tutorials/main/settings.py
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #myapp
+    'tutorial',
+
+    #thirdapp
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- django/projects/Tutorials/tutorial/models.py
+++ django/projects/Tutorials/tutorial/models.py
@@ -3,14 +3,15 @@ from django.db import models
 # Create your models here.
 class Tutorial(models.Model):
   PRIORITY = (
-    (1,'High')
-    (2,'Medium')
+    (1,'High'),
+    (2,'Medium'),
     (3,'Low')
   )
 
   title = models.CharField(max_length=50)
   description = models.CharField(max_length=100)
-  priority = models.Choices(PRIORITY, default=1)
+  priority = models.SmallIntegerField(choices=PRIORITY, default=3)
+  is_done = models.BooleanField(default=False)
   created_date = models.DateTimeField(auto_now=True)
   updated_date = models.DateTimeField(auto_now_add=True)
 
