82e6813f48a464ff429d1eff4eced8abd472fb3b
erhan <erhanserkansabriye@gmail.com>
2022-12-29 11:58:48 +0300

    feat: proje isim değiştirildi

--- django/projects/Tutorials-anthony/tutorial/urls.py
+++ django/projects/Tutorials-anthony/tutorial/urls.py
@@ -4,6 +4,7 @@ from .views import TutorialMVS
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
+#! bu r dökümastasyonda var o yüzden yazdık
 router.register(r"tutorials", TutorialMVS)
 urlpatterns = router.urls
 
