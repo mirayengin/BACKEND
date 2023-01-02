554147998fd5561dc1d3c6d9ad0d3965fe093715
erhan <erhanserkansabriye@gmail.com>
2023-01-02 22:44:29 +0300

    feat : filter serach ve ordering eklendi

--- django/class-notes/010-Blogapp/blog/views.py
+++ django/class-notes/010-Blogapp/blog/views.py
@@ -3,12 +3,21 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Category, Blog
 from .serializers import CategorySerializer, BlogSerializer
 
+# from django_filters.rest_framework import DjangoFilterBackend
+
 
 class CategoryView(ModelViewSet):  # crud
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    filterset_fields = ["name"]
+
+    # filter_backends = [DjangoFilterBackend]
+
 
 
 class BlogView(ModelViewSet):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
+    filterset_fields = ["category_id"]
+    search_fields = ["title","content"]
+    # fieldsets_fields = ["category__name"] #? name e göre filter
