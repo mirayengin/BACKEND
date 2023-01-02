b9b340634fd834631e10eea1cd9a6f2112ab0732
erhan <erhanserkansabriye@gmail.com>
2023-01-02 22:23:15 +0300

    feat : django filter yüklendi

--- django/class-notes/010-Blogapp/main/settings.py
+++ django/class-notes/010-Blogapp/main/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
     #third party
     "rest_framework",
+    "django_filters",
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
