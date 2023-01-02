d1e36f96984aa6bad75a7b1189ee7f22ca702a33
erhan <erhanserkansabriye@gmail.com>
2023-01-02 14:17:08 +0300

    feat: login register logout yapıldı

--- django/projects/BlogApp/user/urls.py
+++ django/projects/BlogApp/user/urls.py
@@ -1,8 +1,11 @@
 
 from django.urls import path, include
-from .views import RegisterView
+from .views import RegisterView, logout
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
-    # path('register/', RegisterView),
+    path('login/', views.obtain_auth_token),
+    path('logout/', logout),
+
 ]
--- django/projects/BlogApp/user/views.py
+++ django/projects/BlogApp/user/views.py
@@ -2,7 +2,32 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from .serializer import RegisterSerializer
 from rest_framework.generics import CreateAPIView
+from rest_framework.authtoken.models import Token
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 # Create your views here.
 class RegisterView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
+
+
+
+    def create(self, request, *args, **kwargs): #! override
+        response = super().create(request, *args, **kwargs)
+        print(response.data)  #!Burdakiler serializer daki fieldsler dir
+
+        token = Token.objects.create(user_id = response.data["id"])
+
+        #! Bu token Token adıyla import ettiğimiz class dan alıyoruz
+        response.data["token"] = token.key
+        print(response.data)  #!Yukarıdakine ek olarak token var
+
+        return response
+
+
+@api_view(['POST'])
+def logout(request):
+    request.user.auth_token.delete()
+    return Response({"message": 'User Logout: Token Deleted'})
